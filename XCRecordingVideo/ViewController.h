4d63ffea4b9140cca584c03639ba9aba4650f8fa
xiaocan <xiaocan@xiaocandemacbook-pro.local>
2017-05-25 17:04:07 +0800

    Initial Commit

---
+++ XCRecordingVideo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XCRecordingVideo
+//
+//  Created by xiaocan on 2017/5/25.
+//  Copyright © 2017年 xiaocan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ XCRecordingVideo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  XCRecordingVideo
+//
+//  Created by xiaocan on 2017/5/25.
+//  Copyright © 2017年 xiaocan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
